f107014dc32e88612f9b1ac461b32bed25346038
silendas <muhammadyasminnulhakim34@gmail.com>
2024-12-17 01:23:06 +0700

    update fix build

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,12 +1,12 @@
 
-export { default as Costumer } from '@/pages/dashboard/Costumer';
-export { default as Tables } from '@/pages/dashboard/Tables';
-export { default as Notifications } from '@/pages/dashboard/Notifications';
-export { default as BarangMasuk } from '@/pages/dashboard/BarangMasuk';
-export { default as BarangKeluar } from '@/pages/dashboard/BarangKeluar';
-export { default as LaporanInventory } from '@/pages/dashboard/LaporanInventory';
-export { default as DaftarGaji } from '@/pages/dashboard/DaftarGaji'; // Ensure this line exists
-export { default as PengelolaanGaji } from '@/pages/dashboard/PengelolaanGaji';
-export { default as LaporanGaji } from '@/pages/dashboard/LaporanGaji';
-export { default as Kasir } from '@/pages/dashboard/Kasir';
-export { default as LaporanPembayaran } from '@/pages/dashboard/LaporanPembayaran';
+export { default as Costumer } from './Costumer';
+export { default as Tables } from './Tables';
+export { default as Notifications } from './Notifications';
+export { default as BarangMasuk } from './BarangMasuk';
+export { default as BarangKeluar } from './BarangKeluar';
+export { default as LaporanInventory } from './LaporanInventory';
+export { default as DaftarGaji } from './DaftarGaji'; // Ensure this line exists
+export { default as PengelolaanGaji } from './PengelolaanGaji';
+export { default as LaporanGaji } from './LaporanGaji';
+export { default as Kasir } from './Kasir';
+export { default as LaporanPembayaran } from './LaporanPembayaran';
