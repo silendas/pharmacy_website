105afbd6497e7e6f4c0936cb1812c0a844908f71
silendas <muhammadyasminnulhakim34@gmail.com>
2024-12-17 01:28:25 +0700

    update fix build

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,12 +1,12 @@
 
-export { Costumer } from './Costumer';
-export { Tables } from './Tables';
-export { Notifications } from './Notifications';
-export { BarangMasuk } from './BarangMasuk';
-export { BarangKeluar } from './BarangKeluar';
-export { LaporanInventory } from './LaporanInventory';
-export { DaftarGaji } from './DaftarGaji'; // Ensure this line exists
-export { PengelolaanGaji } from './PengelolaanGaji';
-export { LaporanGaji } from './LaporanGaji';
-export { Kasir } from './Kasir';
-export { LaporanPembayaran } from './LaporanPembayaran';
+export { default as Costumer } from './Costumer.jsx';
+export { default as Tables } from './Tables.jsx';
+export { default as Notifications } from './Notifications.jsx';
+export { default as BarangMasuk } from './BarangMasuk.jsx';
+export { default as BarangKeluar } from './BarangKeluar.jsx';
+export { default as LaporanInventory } from './LaporanInventory.jsx';
+export { default as DaftarGaji } from './DaftarGaji.jsx'; // Ensure this line exists
+export { default as PengelolaanGaji } from './PengelolaanGaji.jsx';
+export { default as LaporanGaji } from './LaporanGaji.jsx';
+export { default as Kasir } from './Kasir.jsx';
+export { default as LaporanPembayaran } from './LaporanPembayaran.jsx';
